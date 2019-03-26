e7d1b33f6710f82509d0a80db3f2cd145c0f6d5d
Cormac Smith <47928489+CormacS@users.noreply.github.com>
2019-03-26 21:08:34 +0000

    Update Control.java

--- Topic Modeller/src/com/topic/modeller/Control.java
+++ Topic Modeller/src/com/topic/modeller/Control.java
@@ -4,12 +4,7 @@ public class Control {
 
 	public static void main(String[] args) 
 	{
-		FileProcessing f1 = new FileProcessing();
-		f1.Connect();
-		f1.ReadFile();
-		
-		
-		System.out.println("Test Github");
+		GUI gu1 = new GUI("Topic Modeller");
 	}
 
 }
