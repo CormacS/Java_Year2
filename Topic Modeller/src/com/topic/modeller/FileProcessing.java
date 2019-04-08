0b9cecc438955ba1cf26edcfba1ab351a9f08b4b
Cormac Smith <47928489+CormacS@users.noreply.github.com>
2019-04-08 02:03:29 +0100

    Update FileProcessing.java

--- Topic Modeller/src/com/topic/modeller/FileProcessing.java
+++ Topic Modeller/src/com/topic/modeller/FileProcessing.java
@@ -1,84 +1,111 @@
 package com.topic.modeller;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 //import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class FileProcessing 
 {
+	static int yeet = 0;
 	int i = 0;
 	File fleName;
 	File fleName2;
 	File fleName3;
+	Scanner Scanners;
 	Scanner myScanner;
 	Scanner myScanner2;
 	Scanner myScanner3;
 	String temp;
 	String nonowords;
 	boolean stopped = false;
+	File extra = new File("specific.txt");
 
+	
+	List<LinkedHashMap<String,Integer>> list = new ArrayList<LinkedHashMap<String,Integer>>();
 	//HashMap to get a key and a value
 	LinkedHashMap<String, Integer> File1 = new LinkedHashMap<>();
 	LinkedHashMap<String, Integer> File2 = new LinkedHashMap<>();
-	
+
 	public void Connect()
 	{
-		String file = "PokemonHG.txt";
-		String file2 = "League.txt";
+		String file = "ScottPilgrim.txt";
+		String file2 = "Scotty.txt";
 		String stop = "stopwords.txt";
 		
 		try
 		{
 			fleName = new File(file);
 		    fleName2 = new File(file2);
-		    fleName3  = new File(stop);
+		    fleName3 = new File(stop);
 			myScanner = new Scanner(fleName);
 			myScanner2 = new Scanner(fleName2);
 			myScanner3 = new Scanner(fleName3);
+			
 		}
 		catch(Exception e)
 		{
-			System.out.println("No File Exists");
+			System.out.println("I want to Die");
 		}
-		ReadFile();
+		
+		//ReadFile();
 	}
-	
 
 	
-	
-	
-	public void ReadFile()
+	public List<LinkedHashMap<String,Integer>> ReadFile()
 	{
+		if(File1.isEmpty() == false)
+		{
+			File1.clear();
+		}
+		if(File2.isEmpty() == false)
+		{
+			File2.clear();
+		}
+		for(String temp : File2.keySet())
+		{
+			System.out.println(temp+"\n\n\n yeet");
+		}
 		int counter = 0;
+		//Reading in all the stop words into a string
 		try
 		{
 			while(myScanner3.hasNextLine())
 			{
+				String str = myScanner3.nextLine();
 				if(counter == 0)
 				{
-					nonowords = myScanner3.next();
+					nonowords = str;
 					nonowords.replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.,'])","").toLowerCase();
 					counter += 1;
 				}
 				else
 				{					
-					nonowords = nonowords + " " + myScanner3.next();
+					nonowords = nonowords + " "+str;
 					nonowords.replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.,'])","").toLowerCase();
 					
 				}
 				
 			}
-			//System.out.println(nonowords);
+			//Checking the words in file 2 and passing them to the Validate method 
 			while(myScanner2.hasNextLine())
 			{
 				temp = myScanner2.next();
 				temp = temp.replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.,'])","").toLowerCase();
 				
-				if(ValidateWord(temp,myScanner3))
+				boolean extracheck = ValidateWord(temp);
+				if(extracheck == true)
 				{
+					System.out.println("in hereee :o");
 					if(File2.keySet().contains(temp))
 					{
 						File2.put(temp,File2.get(temp)+1);
@@ -90,12 +117,13 @@ public class FileProcessing
 				}
 			}
 			
+			//Checking the words in file 1 and passing them to the Validate method 			
 			while(myScanner.hasNextLine())
 			{
 				temp = myScanner.next();
 				temp = temp.replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.,'])","").toLowerCase();
 
-				if(ValidateWord(temp,myScanner3))
+				if(ValidateWord(temp) == true)
 				{
 					if(File1.keySet().contains(temp))
 					{
@@ -117,38 +145,22 @@ public class FileProcessing
 		
 		catch(Exception e)
 		{
-			System.out.println("Please Connect to a file first");
+			System.out.println(e);
 		}
 		
-		Compare compare= new Compare();
-		compare.sortMap(File1,File2);
-		
-		/*
-		for(String temp : File1.keySet())
-		{
-			int counter1 = File1.get(temp);
-			System.out.println(counter1+" "+temp);
-		}
-		*/
-			
-	/*	for(i=0;i<429;i++)
-		{
-			System.out.println(stopwords[i]);
-		}
-		*/
+		//Calls the Class Compare to Compare the two validate hashmaps
+		list.add(File1);
+		list.add(File2);
+		return list;
+		//Compare compare= new Compare();
+		//compare.sortMap(File1,File2);
 	}
 	
-	public boolean ValidateWord(String word, Scanner myScanner3)
+	public boolean ValidateWord(String word)
 	{
 		String[] stopwords = nonowords.split(" ");
 		boolean Valid = true;
 		i = 0;
-		while(myScanner3.hasNextLine())
-		{
-			stopwords[i] = myScanner3.next();
-			i += 1;
-
-		}
 		for(i=0;i<stopwords.length;i++)
 		{
 			stopwords[i] = stopwords[i].replace(",","");
@@ -157,7 +169,65 @@ public class FileProcessing
 				Valid = false;
 			}
 		}
+		if(yeet != 0 && Valid == true)
+		{
+			String check;
+			try
+			{
+			//	System.out.println("IN here");
+				Scanners = new Scanner(extra);
+				
+				while(Scanners.hasNextLine())
+				{
+					check = Scanners.next();
+					System.out.println(check + word);
+					if(word.equals(check))
+					{
+						Valid = false;
+						System.out.println("\n\n Falseee wrd \n\n" + Valid);
+					}
+				}
+				
+				Scanners.close();
+
+			}
+			catch(Exception e)
+			{
+
+			}
+		}
 		
 		return Valid;
 	}
+	
+	public void extraWords(String word)
+	{
+		try
+		{
+			yeet = yeet + 1;
+            BufferedWriter out = new BufferedWriter(new FileWriter(extra, true)); 
+            out.write(word);
+            out.newLine();
+            out.close(); 
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error writing/creating names.txt");
+		}
+	}
+	
+	public void Clear()
+	{
+		try
+		{
+			PrintWriter writer = new PrintWriter(extra);
+			writer.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("error");
+		}
+	}
+		
 }
+
