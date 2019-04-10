e43abfad23b53b50dc2bd7a39eb4a3415aff42a5
Cormac Smith <47928489+CormacS@users.noreply.github.com>
2019-04-11 00:39:00 +0100

    Update FileProcessing.java

--- Topic Modeller/src/com/topic/modeller/FileProcessing.java
+++ Topic Modeller/src/com/topic/modeller/FileProcessing.java
@@ -16,26 +16,26 @@ import java.util.Scanner;
 
 public class FileProcessing 
 {
-	static int yeet = 0;
-	int i = 0;
-	File fleName;
-	File fleName2;
-	File fleName3;
-	File fleName4;
-	Scanner Scanners;
-	Scanner Scanners2;
-	Scanner myScanner;
-	Scanner myScanner2;
-	Scanner myScanner3;
-	String temp;
-	String nonowords;
-	boolean stopped = false;
-	File extra = new File("specific.txt");
+	private static int yeet = 0;
+	private int i = 0;
+	private File fleName;
+	private File fleName2;
+	private File fleName3;
+	private File fleName4;
+	private Scanner Scanners;
+	private Scanner Scanners2;
+	private Scanner myScanner;
+	private Scanner myScanner2;
+	private Scanner myScanner3;
+	private String temp;
+	private String nonowords;
+	private boolean stopped = false;
+	private File extra = new File("specific.txt");
 
 	
-	List<LinkedHashMap<String,Integer>> list = new ArrayList<LinkedHashMap<String,Integer>>();
-	LinkedHashMap<String, Integer> File1 = new LinkedHashMap<>();
-	LinkedHashMap<String, Integer> File2 = new LinkedHashMap<>();
+	private List<LinkedHashMap<String,Integer>> list = new ArrayList<LinkedHashMap<String,Integer>>();
+	private LinkedHashMap<String, Integer> File1 = new LinkedHashMap<>();
+	private LinkedHashMap<String, Integer> File2 = new LinkedHashMap<>();
 
 	public void Connect(File fleName,File fleName2)
 	{
@@ -43,11 +43,10 @@ public class FileProcessing
 		
 		try
 		{
-		    fleName3 = new File(stop);
-			myScanner = new Scanner(fleName);
-			myScanner2 = new Scanner(fleName2);
-			myScanner3 = new Scanner(fleName3);
-			
+		    setFleName3(new File(stop));
+			setMyScanner(new Scanner(fleName));
+			setMyScanner2(new Scanner(fleName2));
+			setMyScanner3(new Scanner(getFleName3()));
 		}
 		catch(Exception e)
 		{
@@ -58,19 +57,19 @@ public class FileProcessing
 	
 	public List<LinkedHashMap<String,Integer>> ReadFile()
 	{
-		if(File1.isEmpty() == false)
+		if(getFile1().isEmpty() == false)
 		{
-			File1.clear();
+			getFile1().clear();
 			
 		}
-		if(File2.isEmpty() == false)
+		if(getFile2().isEmpty() == false)
 		{
-			File2.clear();
+			getFile2().clear();
 			
 		}
-		if(list.isEmpty() == false)
+		if(getList().isEmpty() == false)
 		{
-			list.clear();
+			getList().clear();
 			
 		}
 
@@ -78,92 +77,92 @@ public class FileProcessing
 		//Reading in all the stop words into a string
 		try
 		{
-			while(myScanner3.hasNext())
+			while(getMyScanner3().hasNext())
 			{
-				String str = myScanner3.nextLine();
+				String str = getMyScanner3().nextLine();
 				if(counter == 0)
 				{
-					nonowords = str;
-					nonowords.replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.!,'])","").toLowerCase();
+					setNonowords(str);
+					getNonowords().replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.!,'])","").toLowerCase();
 					counter += 1;
 				}
 				else
 				{					
-					nonowords = nonowords + " "+str;
-					nonowords.replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.!,'])","").toLowerCase();
+					setNonowords(getNonowords() + " "+str);
+					getNonowords().replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.!,'])","").toLowerCase();
 					
 				}
 				
 			}
 			//Checking the words in file 2 and passing them to the Validate method 
-			while(myScanner2.hasNext())
+			while(getMyScanner2().hasNext())
 			{
-				temp = myScanner2.next();
-				temp = temp.replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.,'])","").toLowerCase();
+				setTemp(getMyScanner2().next());
+				setTemp(getTemp().replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.,'])","").toLowerCase());
 				
-				boolean extracheck = ValidateWord(temp);
+				boolean extracheck = ValidateWord(getTemp());
 				if(extracheck == true)
 				{
 					//System.out.println("in hereee :o");
-					if(File2.keySet().contains(temp))
+					if(getFile2().keySet().contains(getTemp()))
 					{
-						File2.put(temp,File2.get(temp)+1);
+						getFile2().put(getTemp(),getFile2().get(getTemp())+1);
 					}
 					else
 					{
-						File2.put(temp,1);
+						getFile2().put(getTemp(),1);
 					}
 				}
 			}
 			
 			//Checking the words in file 1 and passing them to the Validate method 			
-			while(myScanner.hasNext())
+			while(getMyScanner().hasNext())
 			{
-				temp = myScanner.next();
-				temp = temp.replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.,'])","").toLowerCase();
+				setTemp(getMyScanner().next());
+				setTemp(getTemp().replaceAll("(?:--|[\\\\[\\\\]{}()+/\\\\\\\\.,'])","").toLowerCase());
 
-				if(ValidateWord(temp) == true)
+				if(ValidateWord(getTemp()) == true)
 				{
-					if(File1.keySet().contains(temp))
+					if(getFile1().keySet().contains(getTemp()))
 					{
-						File1.put(temp,File1.get(temp)+1);
+						getFile1().put(getTemp(),getFile1().get(getTemp())+1);
 					}
 					else
 					{
-						File1.put(temp,1);
+						getFile1().put(getTemp(),1);
 					}
 				}
 				
 				
 			}
 			
-			myScanner.close();
-			myScanner2.close();
-			myScanner3.close();
+			getMyScanner().close();
+			getMyScanner2().close();
+			getMyScanner3().close();
 		}
 		
 		catch(Exception e)
 		{
 			System.out.println(e);
 		}
-		list.clear();
+		getList().clear();
 		//Calls the Class Compare to Compare the two validate hashmaps
-		list.add(File1);
-		list.add(File2);
-		return list;
+		getList().add(getFile1());
+		getList().add(getFile2());
+		return getList();
 		//Compare compare= new Compare();
 		//compare.sortMap(File1,File2);
 	}
 	
 	public boolean ValidateWord(String word)
 	{
-		String[] stopwords = nonowords.split(" ");
+		String[] stopwords = getNonowords().split(" ");
 		boolean Valid = true;
-		i = 0;
-		for(i=0;i<stopwords.length;i++)
+		setI(0);
+		for(setI(0);getI()<stopwords.length;i++)
 		{
-			stopwords[i] = stopwords[i].replace(",","");
-			if(word.equals(stopwords[i]))
+			stopwords[getI()] = stopwords[getI()].replace(",","");
+			if(word.equals(stopwords[getI()]))
 			{
 				Valid = false;
 			}
@@ -176,8 +175,8 @@ public class FileProcessing
 	{
 		try
 		{
-			yeet = yeet + 1;
-            BufferedWriter out = new BufferedWriter(new FileWriter(extra, true)); 
+			setYeet(getYeet() + 1);
+            BufferedWriter out = new BufferedWriter(new FileWriter(getExtra(), true)); 
             out.write(word);
             out.newLine();
             out.close(); 
@@ -195,12 +194,12 @@ public class FileProcessing
 		try
 		{
 			//System.out.println("IN here");
-			Scanners = new Scanner(extra);
+			setScanners(new Scanner(getExtra()));
 			//Scanners2 = new Scanner(extra);
 			
-			while(Scanners.hasNextLine())
+			while(getScanners().hasNextLine())
 			{
-				check = Scanners.next();
+				check = getScanners().next();
 				//System.out.println(check + word);
 				if(File.containsKey(check))
 				{
@@ -210,7 +209,7 @@ public class FileProcessing
 			}
 			
 			
-			Scanners.close();
+			getScanners().close();
 
 		}
 		catch(Exception e)
@@ -225,8 +224,8 @@ public class FileProcessing
 		try
 		{
 		    String str = "Output.txt";
-		    fleName4 = new File(str);
-		    BufferedWriter writer = new BufferedWriter(new FileWriter(fleName4, false));
+		    setFleName4(new File(str));
+		    BufferedWriter writer = new BufferedWriter(new FileWriter(getFleName4(), false));
 		    writer.write("");
 		    for(int i = 0;i<10;i++)
 		    {
@@ -252,13 +251,195 @@ public class FileProcessing
 	{
 		try
 		{
-			PrintWriter writer = new PrintWriter(extra);
+			PrintWriter writer = new PrintWriter(getExtra());
 			writer.close();
 		}
 		catch(Exception e)
 		{
 			System.out.println("error");
 		}
+	}
+
+
+	//Setters and Getters
+	
+	public static int getYeet() {
+		return yeet;
+	}
+
+	public static void setYeet(int yeet) {
+		FileProcessing.yeet = yeet;
+	}
+
+
+	public int getI() {
+		return i;
+	}
+
+
+	public int setI(int i) {
+		this.i = i;
+		return i;
+	}
+
+
+	public File getFleName() {
+		return fleName;
+	}
+
+
+	public void setFleName(File fleName) {
+		this.fleName = fleName;
+	}
+
+
+	public File getFleName2() {
+		return fleName2;
+	}
+
+
+	public void setFleName2(File fleName2) {
+		this.fleName2 = fleName2;
+	}
+
+
+	public File getFleName3() {
+		return fleName3;
+	}
+
+
+	public void setFleName3(File fleName3) {
+		this.fleName3 = fleName3;
+	}
+
+
+	public File getFleName4() {
+		return fleName4;
+	}
+
+
+	public void setFleName4(File fleName4) {
+		this.fleName4 = fleName4;
+	}
+
+
+	public Scanner getScanners() {
+		return Scanners;
+	}
+
+
+	public void setScanners(Scanner scanners) {
+		Scanners = scanners;
+	}
+
+
+	public Scanner getScanners2() {
+		return Scanners2;
+	}
+
+
+	public void setScanners2(Scanner scanners2) {
+		Scanners2 = scanners2;
+	}
+
+
+	public Scanner getMyScanner() {
+		return myScanner;
+	}
+
+
+	public void setMyScanner(Scanner myScanner) {
+		this.myScanner = myScanner;
+	}
+
+
+	public Scanner getMyScanner2() {
+		return myScanner2;
+	}
+
+
+	public void setMyScanner2(Scanner myScanner2) {
+		this.myScanner2 = myScanner2;
+	}
+
+
+	public Scanner getMyScanner3() {
+		return myScanner3;
+	}
+
+
+	public void setMyScanner3(Scanner myScanner3) {
+		this.myScanner3 = myScanner3;
+	}
+
+
+	public String getTemp() {
+		return temp;
+	}
+
+
+	public void setTemp(String temp) {
+		this.temp = temp;
+	}
+
+
+	public String getNonowords() {
+		return nonowords;
+	}
+
+
+	public void setNonowords(String nonowords) {
+		this.nonowords = nonowords;
+	}
+
+
+	public boolean isStopped() {
+		return stopped;
+	}
+
+
+	public void setStopped(boolean stopped) {
+		this.stopped = stopped;
+	}
+
+
+	public File getExtra() {
+		return extra;
+	}
+
+
+	public void setExtra(File extra) {
+		this.extra = extra;
+	}
+
+
+	public List<LinkedHashMap<String,Integer>> getList() {
+		return list;
+	}
+
+
+	public void setList(List<LinkedHashMap<String,Integer>> list) {
+		this.list = list;
+	}
+
+
+	public LinkedHashMap<String, Integer> getFile1() {
+		return File1;
+	}
+
+
+	public void setFile1(LinkedHashMap<String, Integer> file1) {
+		File1 = file1;
+	}
+
+
+	public LinkedHashMap<String, Integer> getFile2() {
+		return File2;
+	}
+
+
+	public void setFile2(LinkedHashMap<String, Integer> file2) {
+		File2 = file2;
 	}
 		
 }
